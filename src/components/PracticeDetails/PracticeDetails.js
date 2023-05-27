9e1acb766c3c23d3d158e88ae7f8e176c23999a4
Uzzal Bhowmik <ubcreativity111@gmail.com>
2023-05-27 23:11:53 +0600

    learning time and break time addded

--- src/components/PracticeDetails/PracticeDetails.js
+++ src/components/PracticeDetails/PracticeDetails.js
@@ -1,29 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PracticeDetails.css";
 
 const PracticeDetails = () => {
 
   // break times
+  const [breakTime, setBreakTime] = useState(0);
+
   const breakTimes = [10, 20, 30, 40, 50];
 
-  const handleBreak = (time) => {console.log(time)}
+  const handleBreak = (time) => setBreakTime(time);
+
+
+  // learning time
 
 
   return (
     <div className="practice-details">
 
       {/* Break Time */}
-      <h4 className="fw-bold">Add a Break</h4>
-      <div className="text-center breaks">
-        {
-          breakTimes.map(breakTime => {
-            return (
-              <button key={breakTime} className="break-btn" onClick={()=> handleBreak(breakTime)}>{breakTime}</button>
-            
-            );
-          })
-        }
+      <div>
+        <h4 className="fw-bold">Add a Break</h4>
+        <div className="text-center breaks">
+          {
+            breakTimes.map(breakTime => {
+              return (
+                <button key={breakTime} className="break-btn" onClick={()=> handleBreak(breakTime)}>{breakTime}</button>
+              
+              );
+            })
+          }
+        </div>
       </div>
+
+        
+        {/* Learning Details */}
+        <div className="mt-5">
+          <h4 className="fw-bold">Learning Details</h4>
+
+          <div className="total-time d-flex justify-content-between align-items-center px-3 pt-2 pb-1 mt-4">
+            <h6 className="fw-bold">Learning Time</h6>
+            <h6 className="text-muted">{0} hours</h6>
+          </div>
+
+          <div className="total-time d-flex justify-content-between align-items-center px-3 pt-2 pb-1 mt-2">
+            <h6 className="fw-bold">Break Time</h6>
+            <h6 className="text-muted">{breakTime} hours</h6>
+          </div>
+        </div>
+
+        
+        {/* Activity Completed Button */}
+        <button className="btn btn-primary mt-5 w-100 fw-bold py-2 rounded-3">Activity Completed</button>
+
+
     </div>
   );
 };
